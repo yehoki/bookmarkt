d6a26af22e21c908b7efa92ad7dfd64dfd97c76b
yehoki <patsenn2000@gmail.com>
2023-07-31 20:41:38 +0100

    Fix loading up bookshelves - only show the bookshelves which are currently not chosen

--- frontend/bookmarkt/src/components/Books/SearchBooks/AddBookButton.tsx
+++ frontend/bookmarkt/src/components/Books/SearchBooks/AddBookButton.tsx
@@ -22,10 +22,12 @@ const AddBookButton: React.FC<AddBookButtonProps> = ({
   const [displayBookshelf, setDisplayBookshelf] = useState(currentBookshelf);
   const [bookshelvesWithoutCurrent, setBookshelvesWithoutCurrent] =
     useState(bookshelves);
+  const [isLoading, setIsLoading] = useState(false);
   // fetch if book is read
   const router = useRouter();
 
   const handleChangeBookshelf = async (newBookshelfName: string) => {
+    setIsLoading(true);
     if (displayBookshelf !== '') {
       // When a current bookshelf exist, send it via a PUT request to update
       const res = await fetch(`${SITE_URL}/api/users/bookshelves`, {
@@ -47,13 +49,14 @@ const AddBookButton: React.FC<AddBookButtonProps> = ({
     }
     setDisplayBookshelf(newBookshelfName);
     router.refresh();
+    setIsLoading(false);
   };
 
-  useCallback(() => {
-    const bookshelvesNotCurrent = bookshelves.filter(
+  useEffect(() => {
+    const filteredBookshelves = bookshelves.filter(
       (bookshelf) => bookshelf.name !== displayBookshelf
     );
-    setBookshelvesWithoutCurrent(bookshelvesNotCurrent);
+    setBookshelvesWithoutCurrent(filteredBookshelves);
   }, [displayBookshelf, bookshelves]);
 
   const bookshelfNames = bookshelvesWithoutCurrent.map((bookshelf) => (
@@ -72,44 +75,48 @@ const AddBookButton: React.FC<AddBookButtonProps> = ({
   return (
     <div className="bg-[#409D69]/90 text-[13px] text-white rounded-sm">
       <div className="flex items-center justify-between">
-        <Suspense fallback="...">
-          <div
-            className={`w-[105px] pr-1 pl-2 py-[5px] border-r-[1px] 
+        {isLoading ? (
+          <div className="px-2 py-[5px]">Saving...</div>
+        ) : (
+          <>
+            <Suspense fallback="...">
+              <div
+                className={`w-[105px] pr-1 pl-2 py-[5px] border-r-[1px] 
         font-lato text-left overflow-hidden text-ellipsis whitespace-nowrap
         ${
           currentBookshelf === ''
             ? 'hover:bg-[#409D69] border-neutral-600'
             : 'bg-[#F2F2F2] text-black border-none'
         } rounded-bl-sm rounded-tl-sm`}
-          >
-            {displayBookshelf !== '' ? (
-              <BookshelfButton displayBookshelfName={displayBookshelf} />
-            ) : (
-              <div
-                className="cursor-pointer"
-                onClick={() => handleChangeBookshelf('Want to read')}
               >
-                Want to read
+                {displayBookshelf !== '' ? (
+                  <BookshelfButton displayBookshelfName={displayBookshelf} />
+                ) : (
+                  <div
+                    className="cursor-pointer"
+                    onClick={() => handleChangeBookshelf('Want to read')}
+                  >
+                    Want to read
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-        </Suspense>
-        <div
-          className="px-2 py-2 hover:bg-[#409D69] rounded-tr-sm rounded-br-sm relative group
+            </Suspense>
+            <div
+              className="px-2 py-2 hover:bg-[#409D69] rounded-tr-sm rounded-br-sm relative group
           cursor-pointer"
-          onClick={(e) => {
-            setIsBookshelfDropdown(!isBookshelfDropdown);
-          }}
-          // onMouseEnter={() => setIsBookshelfDropdown(true)}
-          // onMouseLeave={() => {
-          //   setTimeout(() => {
-          //     setIsBookshelfDropdown(false);
-          //   }, 500);
-          // }}
-        >
-          <AiFillCaretDown size={10} className="fill-white" />
-          <div
-            className={`absolute right-0 top-[26px]
+              onClick={(e) => {
+                setIsBookshelfDropdown(!isBookshelfDropdown);
+              }}
+              // onMouseEnter={() => setIsBookshelfDropdown(true)}
+              // onMouseLeave={() => {
+              //   setTimeout(() => {
+              //     setIsBookshelfDropdown(false);
+              //   }, 500);
+              // }}
+            >
+              <AiFillCaretDown size={10} className="fill-white" />
+              <div
+                className={`absolute right-0 top-[26px]
             cursor-auto w-[180px] z-10
           bg-white text-black
             border-[1px] border-neutral-300 rounded-sm
@@ -118,10 +125,12 @@ const AddBookButton: React.FC<AddBookButtonProps> = ({
                 ? 'scale-100 origin-top-right delay-150 duration-300 transform ease-in-out'
                 : 'scale-0 origin-top-right duration-300 transform ease-in-out'
             }`}
-          >
-            <ul className="mt-[2px]">{bookshelfNames}</ul>
-          </div>
-        </div>
+              >
+                <ul className="mt-[2px]">{bookshelfNames}</ul>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
